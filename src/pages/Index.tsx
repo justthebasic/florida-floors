d9d71bd13ec06948ea96dee3fe3cb77c9895c367
Erick <ericklamartinne0800@gmail.com>
2025-06-06 23:56:48 -0300

    first commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import GallerySection from "@/components/GallerySection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import PricingSection from "@/components/PricingSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <GallerySection />
+      <TestimonialsSection />
+      <PricingSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
